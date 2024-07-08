215c276597b24add6c6261b0559f3adfa97c8f91
Ahmed Hassan <ah01157548281@gmail.com>
2024-07-08 12:04:47 -0700

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+#install a flask from pip3
+exec { 'install flask':
+  command =>  'pip3 install flask==2.1.0',
+  path    =>  '/usr/bin/',
+  unless  =>  'pip3 list | grep flask',
 }
