afde00e9e671ab658d36e3bd9e89418be9bf645e
Ahmed Hassan <ah01157548281@gmail.com>
2024-10-03 06:30:57 -0700

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+""" script to obtain subscribers
+    count from a subreddit
+"""
+from requests import get
 
-import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    for the given subreddit. If an invalid subreddit is given, return 0."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    
-    try:
-        # Make the request with no redirects allowed
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if the status code is in the 200 range (valid response)
-        if response.status_code == 200:
-            # Try to extract the number of subscribers
-            data = response.json().get("data", {})
-            return data.get("subscribers", 0)
-        else:
-            return 0  # Invalid subreddit or other error
-    except Exception:
-        # In case of any request errors (network issues, invalid JSON, etc.)
-        return 0
+    """ function to get subscriber count"""
+    if subreddit and type(subreddit) is str:
+        subscribers = 0
+        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'user-agent': 'my-app/0.0.1'}
+        req = get(url, headers=headers)
+        if req.status_code == 200:
+            data = req.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
